46d54a5bd734d6565a71e543583ae96803f496a1
KingCrimson138 <falconahuel094@gmail.com>
2025-09-02 20:46:42 -0300

    Refactor

--- Pokeapp.py
+++ Pokeapp.py
@@ -1,12 +1,16 @@
 import requests
+import sys
 from dataclasses import dataclass
 
-pokeapi = "https://pokeapi.co/api/v2/pokemon/"
+pokeapi = "https://pokeapi.co/api/v2/pokemon/"# Link a 
 typefects = "https://pokeapi.co/api/v2/type/"
 translateApi = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q="
 
 @dataclass
 class Pokemon:
+	'''
+	La clase pokemon 
+	'''
 	name: str
 	pokedexId: int
 	weight: int
@@ -23,10 +27,23 @@ class Pokemon:
 	damage_not_efective: list = None
 	damage_normal: list = None
 
+
 	def to_kg(self):
-		return self.weight/10
+		"""
+		Convierte el peso dado por la api de hectogramo a kilogramo
+		"""
+		self.weight = round(self.weight/10)
+		return True
+	
 
 	def effects(self, url):
+		"""
+		Traduce los efectos obtenidos de la api del ingles al español
+
+		Args:
+			Enlace (url): Enlace a la api para obtener los efectos.
+
+		"""
 		response = requests.get(url)
 		if response.status_code == 200:
 			data = response.json()
@@ -41,9 +58,22 @@ class Pokemon:
 		return True
 
 	def __post_init__(self):
+		'''
+		Inicializa y regula los valores obtenidos por la API
+		'''
+		#if self.height < 100:
+		#	self.to_meter()
+		
+		self.to_kg()
 		self.effects(self.typeurl)
 
 def translate(text):
+	'''
+	Traduce una frase
+
+	Args:
+		Texto (text): Texto a traducir
+	'''
 	query = {
 		"q": text,
 		"source": "es",
@@ -59,6 +89,12 @@ def translate(text):
 
 
 def search_pokemon(pokemon):
+	'''
+	Obtiene los datos de un Pokemon desde la API de PokeAPI.co
+
+	Args:
+		Pokemon (pokemon): Pokemon a encontrar
+	'''
 	response = requests.get(pokeapi+pokemon)
 	if response.status_code == 200:
 		data = response.json()
@@ -76,21 +112,33 @@ def search_pokemon(pokemon):
 			data['stats'][4]['base_stat'],
 			data['stats'][5]['base_stat']
 			)
-	return None
+	return response
 
-sandshrew = search_pokemon("sandshrew")
-
-print(f"Nombre: {sandshrew.name}")
-print(f"PokedexID: {sandshrew.pokedexId}")
-print(f"Peso: {sandshrew.weight}")
-print(f"Altura: {sandshrew.height}")
-print(f"Tipo: {sandshrew.typpe}")
-print(f"Vida base: {sandshrew.hp}")
-print(f"Ataque base: {sandshrew.attack}")
-print(f"Defensa base: {sandshrew.defense}")
-print(f"Ataque especial base: {sandshrew.sp_attack}")
-print(f"Defensa especial base: {sandshrew.sp_defense}")
-print(f"Velocidad: {sandshrew.speed}")
-print(f"Efectivo contra: {sandshrew.damage_effective}")
-print(f"No efectivo contra: {sandshrew.damage_not_efective}")
-print(f"Normal contra: {sandshrew.damage_normal}")
+if __name__ == "__main__":
+	# Si no hay argumentos muestra el uso correcto del programa
+	if len(sys.argv) > 1 and len(sys.argv) < 3:
+		arg = sys.argv[1]
+		poke = search_pokemon(arg)
+		# Si el resultado no es de tipo Pokemon hubo un fallo al buscar
+		if not isinstance(poke, Pokemon):
+			if poke.status_code == 404:
+				print("Pokemon no encontrado")
+		# Muestra el resultado por pantalla
+		else:
+			print(f"Nombre: {poke.name}")
+			print(f"PokedexID: {poke.pokedexId}")
+			print(f"Peso: {poke.weight} kg")
+			print(f"Altura: {poke.height} cm")
+			print(f"Tipo: {poke.typpe}")
+			print(f"Vida base: {poke.hp} hp")
+			print(f"Ataque base: {poke.attack}")
+			print(f"Defensa base: {poke.defense}")
+			print(f"Ataque especial base: {poke.sp_attack}")
+			print(f"Defensa especial base: {poke.sp_defense}")
+			print(f"Velocidad: {poke.speed}")
+			print(f"Efectivo contra: {poke.damage_effective}")
+			print(f"No efectivo contra: {poke.damage_not_efective}")
+			print(f"Normal contra: {poke.damage_normal}")
+    
+	else:
+		print("Uso: Pokeapp nombre_pokemon")
---
+++ README.md
@@ -0,0 +1,33 @@
+# PokeApp ⚡️
+
+Una pequeña app en Python capaz consultar información de Pokémon en **[PokeAPI](https://pokeapi.co/)** y traducir datos de sus tipos al español usando la API de Google Translate.
+
+## 🚀 Funcionalidades
+
+- Buscar un Pokémon por nombre en la **PokeAPI**.
+- Mostrar información detallada:
+  - Nombre
+  - Número en la Pokédex
+  - Peso (convertido a kilogramos)
+  - Altura en cm
+  - Tipo
+  - Estadísticas base (HP, ataque, defensa, etc.)
+- Mostrar **efectividad del tipo**:
+  - Tipos contra los que es **efectivo**
+  - Tipos contra los que es **no efectivo**
+  - Tipos contra los que tiene **efectividad normal**
+- Traducción automática de tipos de Pokémon al español.
+
+## 📦 Requisitos
+
+- Python 3.8 o superior
+- Librerías:
+  - `requests`
+  - `dataclasses` (incluida en Python 3.7+)
+
+Instalación de dependencias:
+ ``pip install .``
+
+ ###⚠️ Notas
+El programa utiliza una API de traduccion no estable asi que puede estar sujeto
+a un repentino malfuncionamiento
